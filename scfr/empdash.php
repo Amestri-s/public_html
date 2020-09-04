57e303370a31c4cae2d69fd8ab97a5e6a42bcf94
Amestri-s <btamestris@gmail.com>
2020-09-04 10:39:08 +0100

    Added patrol log modal

--- scfr/empdash.php
+++ scfr/empdash.php
@@ -29,6 +29,28 @@ $resources = mysqli_query($link, 'SELECT name, description, resLink FROM resourc
 mysqli_close($link);
 
 ?>
+<!-- Modal -->
+<div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+  aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 <div class="jumbotron">
     <h3 class="display-4 animated fadeIn text-center">Quick actions</h3>
@@ -43,7 +65,9 @@ mysqli_close($link);
     <div class="card-body">
 
         <!--Title-->
-        <h4 class="card-title text-center">Coming soon</h4>
+        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#basicExampleModal">
+          Submit patrol log
+        </button>
         <!--Text-->
 
     </div>
